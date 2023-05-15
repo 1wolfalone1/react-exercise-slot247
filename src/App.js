f923684a3edaaff3826354d0d4247052c58fed7d
NNhatTThien <thientryhard@gmail.com>
2023-05-15 20:22:15 +0700

    Using the map() function and return to JSX for display all Players.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./component/navigation/Navigation";
-import Players from "./component/player/Players";
+import Players from "./component/players/Players";
 import Footer from "./component/footer/Footer";
 
 function App() {
