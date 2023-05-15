bcf5a854ad8ae6e9d6aa365e3d80b785fce3fad6
NNhatTThien <thientryhard@gmail.com>
2023-05-15 20:32:55 +0700

    Functional Components

--- src/component/navigation/Navigation.jsx
+++ src/component/navigation/Navigation.jsx
@@ -1,28 +1,25 @@
 import React, { Component } from "react";
-import './Navigation.css'
-export default class Navigation extends Component {
-   render() {
-      return (
-         <div className="nav-container">
-            <nav>
-               <ul>
-                  <li className="active">
-                     <a  href="#home">
-                        Home
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#news">News</a>
-                  </li>
-                  <li>
-                     <a href="#about">About</a>
-                  </li>
-                  <li>
-                     <a href="#contact">Contact</a>
-                  </li>
-               </ul>
-            </nav>
-         </div>
-      );
-   }
+import "./Navigation.css";
+export default function Navigation() {
+
+   return (
+      <nav className="nav-container">
+         <ul>
+            <li  className="active">
+               <a href="#home">
+                  Home
+               </a>
+            </li>
+            <li>
+               <a href="#news">News</a>
+            </li>
+            <li>
+               <a href="#about">About</a>
+            </li>
+            <li>
+               <a href="#contact">Contact</a>
+            </li>
+         </ul>
+      </nav>
+   );
 }
--- src/component/players/Players.jsx
+++ src/component/players/Players.jsx
@@ -2,23 +2,21 @@ import React, { Component } from "react";
 import { ListPlayers } from "../../assets/store/ListOfPlayers.js";
 import "./players.css";
 
-export default class Players extends Component {
-   render() {
-      return (
-         <div className="container">
-            {ListPlayers.map((player) => (
-               <div className="column" >
-                  <div className="card">
-                     <img src={require(`../../${player.img}`)} alt="" />
-                     <h3>{player.name}</h3>
-                     <p className="title">{player.club}</p>
-                     <p>
-                        <button>Detail</button>
-                     </p>
-                  </div>
+export default function Players() {
+   return (
+      <div className="container">
+         {ListPlayers.map((player) => (
+            <div className="column">
+               <div className="card">
+                  <img src={require(`../../${player.img}`)} alt="" />
+                  <h3>{player.name}</h3>
+                  <p className="title">{player.club}</p>
+                  <p>
+                     <button>Detail</button>
+                  </p>
                </div>
-            ))}
-         </div>
-      );
-   }
+            </div>
+         ))}
+      </div>
+   );
 }
