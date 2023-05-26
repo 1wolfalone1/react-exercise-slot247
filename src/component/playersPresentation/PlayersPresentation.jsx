ed2102d4f7b71964d1294572a5008dafc49a3f49
NNhatTThien <thientryhard@gmail.com>
2023-05-26 08:58:34 +0700

    Using useState to present popup details of player

--- src/component/playersPresentation/PlayersPresentation.jsx
+++ src/component/playersPresentation/PlayersPresentation.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./players.css";
 export default function PlayersPresentation({ players }) {
-   //console.log(players);
+   const [player, setPlayer] = useState([]);
+   console.log(player);
    return (
       <div className="container">
          {players.map((player) => (
-            <div className="column">
+            <div className="column" key={player.id}>
                <div className="card">
-                  <img src={require(`../../${player.img}`)} alt=""/>
+                  <img src={`../../${player.img}`} alt="" />
                   <h3>{player.name}</h3>
                   <p className="title">{player.club}</p>
                   <p>
-                     <button>Detail</button>
+                     <button
+                        onClick={() => {
+                           setPlayer(player);
+                        }}
+                     >
+                        <a
+                           href="#popup"
+                           id="openPopUp"
+                           style={{ color: "green", width: "100%" }}
+                        >
+                           Detail
+                        </a>
+                     </button>
                   </p>
                </div>
             </div>
          ))}
+         <div id="popup" className="overlay">
+            <div className="popup">
+               <img src={player.img} alt="" />
+               <h2>{player.name}</h2>
+               <a className="close" href="#">
+                  &times;
+               </a>
+               <div className="content">{player.info}</div>
+            </div>
+         </div>
       </div>
    );
 }
