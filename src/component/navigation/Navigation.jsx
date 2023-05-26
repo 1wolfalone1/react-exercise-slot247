66ade184b1cdd1ab81f9c24a7cb5e7486a42ea05
NNhatTThien <thientryhard@gmail.com>
2023-05-26 09:21:35 +0700

    use Effect & Context Hook in your app to custom style mode

--- src/component/navigation/Navigation.jsx
+++ src/component/navigation/Navigation.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import "./Navigation.css";
+import { ThemeContext } from "../theme/ThemeContext";
 export default function Navigation() {
-
+   const { theme, toggle, dark } = useContext(ThemeContext);
    return (
-      <nav className="nav-container">
+      <nav
+         className="nav-container"
+         style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+      >
          <ul>
-            <li  className="active">
-               <a href="#home">
-                  Home
-               </a>
+            <li className="active">
+               <a href="#home">Home</a>
             </li>
             <li>
                <a href="#news">News</a>
@@ -20,6 +22,21 @@ export default function Navigation() {
                <a href="#contact">Contact</a>
             </li>
          </ul>
+         <div style={{ position: "relative", marginLeft: 'auto' }}>
+            <button
+               className="switch-mode"
+               href="#"
+               onClick={toggle}
+               style={{
+                  backgroundColor: theme.backgroundColor,
+                  color: theme.color,
+                  outline: "none",
+               }}
+               data-testid="toggle-theme-btn"
+            >
+               Switch Nav to {!dark ? "Dark" : "Light"} mode
+            </button>
+         </div>
       </nav>
    );
 }
