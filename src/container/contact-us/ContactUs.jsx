1bb68b2e627eccc8fab0e966c10e4011cee1f4c6
NNhatTThien <thientryhard@gmail.com>
2023-06-16 20:04:53 +0700

    learnt to use Formik and Yup to contact us form

--- src/container/contact-us/ContactUs.jsx
+++ src/container/contact-us/ContactUs.jsx
@@ -5,23 +5,54 @@ import {
    Box,
    Button,
    FormControl,
+   FormControlLabel,
    InputLabel,
    MenuItem,
    Select,
+   Switch,
    TextField,
    Typography,
 } from "@mui/material";
+import { useFormik } from "formik";
 import React, { useState } from "react";
+import * as Yup from "yup";
 const styled = (theme) => {
    return {
       select: {},
    };
 };
 export default function ContactUs() {
-   const [selected, setSelected] = useState(0);
-   const handleSelect = (e) => {
-      setSelected(e.target.value);
-   }
+   const formik = useFormik({
+      initialValues: {
+         name: "",
+         email: "",
+         phone: "",
+         nation: 0,
+         content: "",
+         agree: false,
+      },
+      validateOnChange: true,
+      validateOnBlur: true,
+      onSubmit: (values)=>{alert(JSON.stringify(formik.values))},
+      validationSchema: Yup.object({
+         name: Yup.string()
+            .required("Required.")
+            .min(2, "Must be 2 characters or more"),
+         phone: Yup.number().integer().typeError("Please enter a valid number"),
+         email: Yup.string().required("Required.").email("Invalid email"),
+         nation: Yup.number()
+            .integer()
+            .typeError("Please select a favorite nation."),
+         content: Yup.string()
+            .required("Required.")
+            .min(10, "Must be 10 characters or more"),
+         agree: Yup.boolean().oneOf(
+            [true],
+            "The terms and conditions must be accepted."
+         ),
+      }),
+   });
+   console.log(formik.touched)
    const theme = useTheme();
    const s = styled(theme);
    return (
@@ -33,45 +64,67 @@ export default function ContactUs() {
             padding: 8,
          }}
       >
-       
          <form
-            action=""
             style={{
                display: "flex",
                flexDirection: "column",
-               gap: '20px',
-               width: '50%',
-               padding: '10px 20px',
-               border: '1px solid ' + theme.palette.brands.main
+               gap: "20px",
+               width: "50%",
+               padding: "10px 20px",
+               border: "1px solid " + theme.palette.brands.main,
             }}
+            onSubmit={formik.handleSubmit}
          >
-              <Typography color='text.brands' variant="h3">Contact us</Typography>
+            <Typography color="text.brands" variant="h3">
+               Contact us
+            </Typography>
             <TextField
                id="name"
                name="name"
                label="Your name"
                sx={s}
                variant="filled"
+               value={formik.values.name}
+               onChange={formik.handleChange}
             />
+            {formik.errors.name && (
+               <Typography variant="caption" color="red">
+                  {formik.errors.name}
+               </Typography>
+            )}
             <TextField
                id="phone"
                name="phone"
                label="Your phone"
                variant="filled"
+               value={formik.values.phone}
+               onChange={formik.handleChange}
             />
+            {formik.errors.phone && (
+               <Typography variant="caption" color="red">
+                  {formik.errors.phone}
+               </Typography>
+            )}
             <TextField
                id="email"
                name="email"
                label="Your email"
                variant="filled"
+               value={formik.values.email}
+               onChange={formik.handleChange}
             />
+            {formik.errors.email && (
+               <Typography variant="caption" color="red">
+                  {formik.errors.email}
+               </Typography>
+            )}
             <FormControl variant="filled" sx={{ minWidth: 120 }}>
                <Select
                   labelId="demo-simple-select-filled-label"
                   id="demo-simple-select-filled"
                   sx={s.select}
-                  value={selected}
-                  onChange={handleSelect}
+                  value={formik.values.nation}
+                  onChange={(e) => formik.setFieldValue('nation', e.target.value)}
                   MenuProps={{
                      disableScrollLock: true,
                      PaperProps: {
@@ -90,22 +143,54 @@ export default function ContactUs() {
                   <MenuItem value={2}>France</MenuItem>
                   <MenuItem value={3}>Spain</MenuItem>
                </Select>
+               {formik.errors.nation && (
+                  <Typography variant="caption" color="red">
+                     {formik.errors.nation}
+                  </Typography>
+               )}
             </FormControl>
-            <Box sx={{ display: "flex", alignItems: "center" , gap: 1}}>
-               <BorderColorIcon color="primary" />
+            <Box sx={{ display: "flex", alignItems: "start", gap: 1 }}>
+               <BorderColorIcon color="primary" mt={2} />
                <TextField
                   id="content"
                   name="content"
                   label="Your content"
                   variant="filled"
                   multiline
-                  rows={1}
-                  rowsMax={100}
-                  sx={{flex: 1}}
+                  rows={4}
+                  sx={{ flex: 1 }}
+                  value={formik.values.content}
+                  onChange={formik.handleChange}
                />
+               
             </Box>
-            <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
-               <Button variant="outlined" sx={{fontSize: 24}}>Submit</Button>
+            {formik.errors.content && (
+                  <Typography variant="caption" color="red">
+                     {formik.errors.content}
+                  </Typography>
+               )}
+            <FormControlLabel
+               control={<Switch />}
+               color="text.main"
+               label={
+                  <Typography color="text.brands">
+                     Agree to terms and conditions.
+                  </Typography>
+               }
+               name="agree"
+               value={formik.values.agree}
+               onClick={formik.handleChange}
+            />
+            {formik.errors.agree && (
+               <Typography variant="caption" color="red">
+                  {formik.errors.agree}
+               </Typography>
+            )}
+
+            <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+               <Button variant="outlined" sx={{ fontSize: 24 }} type='submit'>
+                  Submit
+               </Button>
             </Box>
          </form>
       </Box>
