36e4f58be9a997729b925f1a824834d05d0c03c9
NNhatTThien <thientryhard@gmail.com>
2023-06-16 18:30:45 +0700

    learnt to use React with Redux and Redux Toolkit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import AddUserForm from "./component/AddUserForm";
+import ListUser from "./container/ListUser";
+import Header from "./component/Header";
+import { Box, Stack } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <>
+         <Box
+            sx={{
+               backgroundColor: "#dadada",
+               minHeight: '100vh'
+            }}
+         >
+            <Header />
+            <Grid2
+               container
+               spacing={4}
+               sx={{
+                  padding: 5,
+               }}
+            >
+               <Grid2 xs={4}>
+                  <AddUserForm />
+               </Grid2>
+               <Grid2 xs={8} >
+                  <ListUser />
+               </Grid2>
+            </Grid2>
+         </Box>
+      </>
+   );
 }
 
 export default App;
